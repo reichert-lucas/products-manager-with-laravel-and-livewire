ec94c4d36173272dbba974e81c4010ab8c435abe
Lucas Coelho Reichert <lucas3d4783@gmail.com>
2021-05-18 12:16:36 -0300

    Seeder de Usuarios; Atualizacao gitignore

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,17 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
-        //
+        User::factory()
+                ->create([
+                    'name' => 'Lucas Reichert',
+                    'email' => 'lucas.reichert@redes.ufsm.br',
+                    'password' => bcrypt('password'),
+                ]);
+        User::factory()->times(10)->create();
     }
 }
